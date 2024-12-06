c138d5aeb9b02b26163f976959c074c2fdb6c38a
Daniela Wiepert <daniela.wiepert7@gmail.com>
2024-12-06 00:38:10 -0600

    Update __init__.py

--- audio_features/io/__init__.py
+++ audio_features/io/__init__.py
@@ -1,8 +1,6 @@
 from ._save_feats import *
 from ._load_feats import *
 from ._split_feats import *
-from ._load_phone_identity import *
-from ._load_word_identity import *
 from ._train_val_test_splits import *
 from ._align_times import *
 from ._load_identities import *
@@ -10,8 +8,6 @@ from ._load_identities import *
 __all__ = ['save_features',
            'load_features',
            'split_features',
-           'phoneIdentity',
-           'wordIdentity',
            'Identity',
            'DatasetSplitter',
            'align_times']
